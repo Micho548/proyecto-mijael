638812b6d1f360cd275d78e098cd061c2eaa5466
limbert aruquipa <limbert0226@gmail.com>
2020-08-23 19:07:23 -0400

    Se unio html con js con varios ejemplos

---
+++ limbert-aruquipa/proyecto/js/script.js
@@ -0,0 +1,13 @@
+alert("HOLA PAPOS");
+
+var persona={
+    nombre:"Limbert",
+    apellido:"Aruquipa",
+    estatura:1.75,
+    colorOjos: "Brown"
+};
+
+document.getElementById("elemento4").innerHTML = persona.nombre + " it's my name " + 
+persona.apellido + " it's my last Name" + 
+persona.estatura + " my stature " + 
+persona.colorOjos + " it's the color of my eyes";
