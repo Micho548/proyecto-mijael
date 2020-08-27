14b37e897bfa88e1475bb62dab0e9915c8d8f56c
Fidel <Toranzos.E.Fidel.G@gmail.com>
2020-08-27 19:04:53 -0400

    Nueva Actualizacion

--- fidel-toranzos/Examen-practica2/JavaScript/JavaMuebles.js
+++ fidel-toranzos/Examen-practica2/JavaScript/JavaMuebles.js
@@ -5,3 +5,39 @@ function abrir(){
 function cerrar(){
     document.getElementById("vent").style.display="none";
 }
+
+$(document).ready(function () {
+    $('#tablax').DataTable({
+        language: {
+            processing: "Tratamiento en curso...",
+            search: "Buscar&nbsp;:",
+            lengthMenu: "Agrupar de _MENU_ items",
+            info: "Mostrando del item _START_ al _END_ de un total de _TOTAL_ items",
+            infoEmpty: "No existen datos.",
+            infoFiltered: "(filtrado de _MAX_ elementos en total)",
+            infoPostFix: "",
+            loadingRecords: "Cargando...",
+            zeroRecords: "No se encontraron datos con tu busqueda",
+            emptyTable: "No hay datos disponibles en la tabla.",
+            paginate: {
+                first: "Primero",
+                previous: "Anterior",
+                next: "Siguiente",
+                last: "Ultimo"
+            },
+            aria: {
+                sortAscending: ": active para ordenar la columna en orden ascendente",
+                sortDescending: ": active para ordenar la columna en orden descendente"
+            }
+        },
+        scrollY: 540,
+        lengthMenu: [ [10, 25, -1], [10, 25, "All"] ],
+    });
+});
+
+function mostrar(input){
+    var img=document.getElementById("img");
+    if(input.value=="Armario Contemporaneo"){
+        img.style.visibility="visible";
+    }
+}
