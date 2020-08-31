bb75a50b584d7be3cbc42a6477e3ca627acc04f5
Eynar Baltazar <eynarbaltazar@gmail.com>
2020-08-31 17:20:16 -0400

    Examen final

---
+++ eynar-baltazar/Examen Final/js/java.js
@@ -0,0 +1,27 @@
+
+function miFuncion(){ 
+        //hago algo... 
+        alert("Registered") 
+        x.style.color = "orange";
+}
+
+function micompra(){ 
+        //hago algo... 
+        alert("To make a purchase please register") 
+        x.style.color = "orange";
+}
+function intente(){ 
+        //hago algo... 
+        alert("Try later (connection problem)") 
+        x.style.color = "orange";
+}
+function bienvenido(){ 
+        //hago algo... 
+        alert("Welcome") 
+        x.style.color = "orange";
+}
+function enviar(){ 
+        //hago algo... 
+        alert("Sent successfully") 
+        x.style.color = "orange";
+}
