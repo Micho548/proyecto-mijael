5599451ffd8212b5888f758d0d9b6881e3e12676
limbert aruquipa <limbert0226@gmail.com>
2020-08-31 18:24:15 -0400

    Ingrese al archivo PROYECTO para la revision examen practico

--- limbert-aruquipa/proyecto/js/script.js
+++ limbert-aruquipa/proyecto/js/script.js
@@ -1,5 +1,14 @@
-alert("HOLA PAPOS");
-
+/* para elegir con clik en un buscador */
+$(function(){
+    $(".dropdown-item").click(function(){ /* debemos selecionar el nombre de items del buscador */
+        var icon_text=$(this).html(); 
+        $(".jsl").html(icon_text);     /* selecionamos el nombre del boton buscador */
+    })
+})
+$(function(){
+    alert("Ing lo hize en ENGLISH por que me estoy familiarizando con el Idioma");
+})
+/*
 var persona={
     nombre:"Limbert",
     apellido:"Aruquipa",
@@ -11,3 +20,4 @@ document.getElementById("elemento4").innerHTML = persona.nombre + " it's my name
 persona.apellido + " it's my last Name" + 
 persona.estatura + " my stature " + 
 persona.colorOjos + " it's the color of my eyes";
+*/
